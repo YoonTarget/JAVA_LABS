7350823eab01a7170e94a9c47594ee1278194044
YoonTarget <126389803+YoonTarget@users.noreply.github.com>
2023-04-20 23:38:07 +0900

    My_First_Java

---
+++ DoubleEx.java
@@ -0,0 +1,21 @@
+package chapter2;
+
+public class DoubleEx {
+	
+	int abc;
+
+	public static void main(String[] args) {
+
+		var dNum = 3.14;
+		float fNum = 3.14F;
+		
+		System.out.println(dNum);
+		
+		var num = 10;
+		System.out.println(num);
+		
+		//num = 3.14;
+		
+	}
+
+}
