ae470593fafaf07e3ef95ed0e2fa54b785213cfc
YoonTarget <126389803+YoonTarget@users.noreply.github.com>
2023-04-29 18:39:06 +0900

    modify logic

---
+++ ReverseTriangle.java
@@ -0,0 +1,21 @@
+package star;
+
+public class ReverseTriangle {
+
+	public static void main(String[] args) {
+		
+		int n = 5;
+		
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < i; j++ ) {
+				System.out.print(" ");
+			}
+			for(int j = 0; j < (2*n - 1) - 2*i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
