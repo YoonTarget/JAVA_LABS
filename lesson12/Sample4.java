18e1eaed9b54603799734cbffa6a808fffb6ca59
YoonTarget <126389803+YoonTarget@users.noreply.github.com>
2023-05-23 16:49:43 +0900

    5/23

--- lesson12/Sample4.java
+++ lesson12/Sample4.java
@@ -3,16 +3,16 @@ package lesson12;
 
 //탈 것 인터페이스
 interface iVehicle2 {
-	void vShow();
+	void vShow(); //iVehicle의 추상 메소드입니다
 }
 
 //목재 인터페이스
 interface iMaterial {
-	void mShow();
+	void mShow(); //iMaterial의 추상 메소드입니다
 }
 
 //자동차 클래스
-class Car4 implements iVehicle2, iMaterial {
+class Car4 implements iVehicle2, iMaterial { //2개 이상의 인터페이스를 구현할 수 있습니다
 	private int num;
 	private double gas;
 	
@@ -24,10 +24,12 @@ class Car4 implements iVehicle2, iMaterial {
 	
 	public void vShow() {
 		System.out.println("차량 번호는 " + num + ", 연료 양은 " + gas + "입니다.");
+		//iVehicle 메소드를 정의합니다
 	}
 	
 	public void mShow() {
 		System.out.println("자동차의 재질은 철입니다.");
+		//iMaterial 메소드를 정의합니다
 	}
 }
 
